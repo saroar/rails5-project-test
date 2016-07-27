adc7fccb14afb062523d3b17d87819451182ef86
Saroar <saroar9@gmail.com>
2016-07-27 17:26:41 +0300

    update migration

--- db/migrate/20160726140405_add_default_fields.rb
+++ db/migrate/20160726140405_add_default_fields.rb
@@ -1,8 +1,8 @@
 class AddDefaultFields < ActiveRecord::Migration[5.0]
   def change
-    add_column :working_estimates, :default_sw_ids, :integer, array: true, default: [], null: false
+    add_column :working_estimates, :default_sw_ids, :integer, default: [], null: false, array: true
 
-    add_column :sub_workings, :default_sp_ids, :integer, array: true, default: [], null: false
+    add_column :sub_workings, :default_sp_ids, :integer, default: [], null: false, array: true
 
   end
 end
